b08f03d53be27c2de265b12dd3a655868ee74e56
brooke-kinsey <kinseb2@rpi.edu>
2025-12-01 15:19:16 -0500

    Update assignment3.R

--- assignment3/assignment3.R
+++ assignment3/assignment3.R
@@ -14,6 +14,24 @@ library(randomForest)
 library(dplyr)
 library(tidyr)
 
-setwd("C:/Users/brook/Downloads/Data Science/assignment3")
+setwd("C:/Users/brook/Downloads/DataScience/assignment3")
 
 house.data <- read.csv("real_estate_dataset.csv")
+
+# EDA
+summary(house.data$Square_Feet)
+summary(house.data$Num_Bathrooms)
+summary(house.data$Num_Bedrooms)
+summary(house.data$Location_Score)
+summary(house.data$Distance_to_Center)
+summary(house.data$Year_built)
+
+
+hist(house.data$Square_Feet)
+hist(house.data$Num_Bathrooms)
+hist(house.data$Num_Bedrooms)
+hist(house.data$Location_Score)
+hist(house.data$Distance_to_Center)
+
+ggplot()
+
